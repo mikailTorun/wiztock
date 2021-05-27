15638624288ef642d8818e272402ab0e1d1bedc3
Enver Aslan <enveraslan5851@gmail.com>
2021-05-27 17:23:22 +0300

    Add unit model and unit service write the necessary functions

---
+++ src/app/models/unit.ts
@@ -0,0 +1,6 @@
+export class Unit {
+  uom_id!: number;
+  company_id!: number;
+  title!: string;
+  description!: string;
+}
---
+++ src/app/services/unit.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@angular/core';
+import {Unit} from "../models/unit";
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environments/environment";
+import {map} from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UnitService {
+  units!: Unit[];
+  constructor(
+    private http:HttpClient
+  ) { }
+
+  addUnit(unit: Unit) {
+    let formData = new FormData();
+    formData.append('unit_of_measurement', JSON.stringify(unit));
+    formData.append('func', "addUnit");
+    return this.http.post(environment.apiUrl, formData)
+      .pipe(map((res: any)=> {
+        return res;
+      }));
+  }
+
+  updateUnit(unit: Unit) {
+    let formData = new FormData();
+    formData.append('unit_of_measurement', JSON.stringify(unit));
+    formData.append('func', "updateUnit");
+    return this.http.post(environment.apiUrl, formData)
+      .pipe(map((res: any)=> {
+        return res;
+      }));
+  }
+
+  deleteUnit(uom_id: number) {
+    let formData: any = new FormData();
+    formData.append("uom_id", uom_id);
+    formData.append("func", "deleteUnit");
+    return this.http.post(environment.apiUrl, formData)
+      .pipe(map ((res: any) => {
+        return res;
+      }));
+  }
+
+  getAllUnit() {
+    let formData: any = new FormData();
+    formData.append("func", "getAllUnit");
+    this.http.post(environment.apiUrl, formData)
+      .subscribe((res: any) => {
+        this.units = res["data"];
+        console.log(this.units);
+      });
+  }
+
+  getUnitById(uom_id: number) {
+    let formData: any = new FormData();
+    formData.append("uom_id", uom_id);
+    formData.append("func", "getUnitById");
+    return this.http.post(environment.apiUrl, formData)
+      .pipe(map ((res: any) => {
+        return res["data"][0];
+      }));
+  }
+}
+
