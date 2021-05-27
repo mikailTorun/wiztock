22057cbcc48311b9c8c5905cfc6f793af57d7c2f
Enver Aslan <enveraslan5851@gmail.com>
2021-05-27 14:32:26 +0300

    Revise the category component functions and add the warning message for category title

--- src/app/modules/categories/categories.component.ts
+++ src/app/modules/categories/categories.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Category} from "../../models/category";
 import {CategoryService} from "../../services/category.service";
+import {Toasts} from "../../helpers/toasts";
+
+declare var $: any;
 
 @Component({
   selector: 'app-categories',
@@ -9,47 +12,101 @@ import {CategoryService} from "../../services/category.service";
 })
 export class CategoriesComponent implements OnInit {
   category: Category = new Category();
+  isEmptyField: boolean = false;
   constructor(
     public categoryService: CategoryService
-  ) { }
+  ) {
+  }
 
   ngOnInit(): void {
     this.getCategoriesList();
+  }
+
+  clearField() {
+    this.category = new Category();
+    this.isEmptyField = false;
+  }
+
+  newCategoryButtonHandler() {
+    $('#categoryForm').modal('show');
+    this.clearField();
+  }
+
+  editCategoryButtonHandler(category_id: number) {
+    $('#categoryForm').modal('show');
+    this.getCategory(category_id);
+  }
+
+  deleteCategoryButtonHandler(category: Category) {
+    this.deleteCategory(category);
+  }
+
+  closeCategoryButtonHandler() {
+    $('#categoryForm').modal('hide');
+  }
+
+  async saveButtonHandler() {
+    this.checkEmptyFields();
+    if(!this.isEmptyField) {
+      if (this.category.product_category_id) {
+        this.updateCategory();
+      } else {
+        this.saveCategory();
+      }
+    }
+  }
+
+  getCategory(category_id: number) {
+    this.categoryService.getCategoryById(category_id).subscribe((res: Category) => {
+      this.category = res;
+    });
   }
 
   getCategoriesList() {
     this.categoryService.getAllCategories();
   }
 
-  addCategoryButtonHandler() {
-    if(this.category.product_category_id) {
-      this.updateCategory();
-    } else {
-      this.saveCategory();
-    }
-    this.getCategoriesList();
-    this.clearFieldButtonHandler();
-  }
-
-  getCategory(category_id: number) {
-    this.categoryService.getCategoryById(category_id).subscribe((res:Category) => {
-      this.category = res;
+  saveCategory() {
+    this.categoryService.addCategory(this.category).subscribe((res: any) => {
+      if (res["success"]) {
+        Toasts.successToast("A new category is added");
+        this.getCategoriesList();
+        this.clearField();
+        this.closeCategoryButtonHandler();
+      }
     });
   }
 
-  deleteCategoryButtonHandler(product_category_id: number) {
-    this.categoryService.deleteCategory(product_category_id);
-  }
-
-  saveCategory() {
-    this.categoryService.addCategory(this.category);
-  }
-
   updateCategory() {
-    this.categoryService.updateCategories(this.category);
+    return this.categoryService.updateCategories(this.category).subscribe((res: any) => {
+      if (res["success"]) {
+        Toasts.successToast("The category " + this.category.title + " was updated");
+        this.getCategoriesList();
+        this.clearField();
+        this.closeCategoryButtonHandler();
+      }
+    });
   }
 
-  clearFieldButtonHandler() {
-    this.category = new Category();
+  deleteCategory(category: Category) {
+    return this.categoryService.deleteCategory(category.product_category_id).subscribe((res: any) => {
+      if (res["success"]) {
+        Toasts.dangerToast( "The category " + category.title + " was deleted" );
+        this.getCategoriesList();
+        this.clearField();
+        this.closeCategoryButtonHandler();
+      }
+    });
+  }
+
+
+  checkEmptyFields() {
+    if(!this.category.title) {
+      this.isEmptyField = true;
+      console.log(true);
+    } else {
+      this.isEmptyField =false;
+      console.log(false);
+    }
   }
 }
